0734b7282f5662b6430434ec39e2b5d344884ede
marco <dobermannpoker86@gmail.com>
2020-03-23 17:39:31 +0100

    non proprio finito

---
+++ giochini.js
@@ -0,0 +1,42 @@
+//1 Chiedi all’utente la sua email controlla che sia nella lista di chi può accedere e stampa un messaggio appropriato;
+
+// chiediamo l email all utente
+
+
+var email = prompt("inserisca email");
+
+//fare lista email consentite
+
+var lista = ["google", "yahoo", "tiscali" , "outlook"];
+
+// dare lunghezza a email e lista
+var listetta = lista.length;
+
+var lunghezzaEmail = email.length;
+
+// titolo per risultato
+var risultatoGiusto;
+
+
+
+// facciamo ciclo
+
+ for (var i =0; i < lista.length ; i++)
+
+    if (lunghezzaEmail>= listetta ) {
+
+        risultatoGiusto = "ok";
+        console.log(lista[i]);
+
+} 
+else  {
+
+        risultatoGiusto = "error";
+        console.log(risultatoGiusto);
+}
+
+
+
+// stampare poi un messaggio
+
+document.getElementById('games').innerHTML="accesso corretto? : " + risultatoGiusto ;
